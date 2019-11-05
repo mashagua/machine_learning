3a5b785d0250a6ea88850c224750a8d18197672e
mashagua <993498039@qq.com>
2019-11-05 21:25:31 +0800

    add files

---
+++ Decision_Tree_ex1.py
@@ -0,0 +1,91 @@
+import numpy as np
+
+
+class Node:
+    def __init__(
+            self,
+            predicted_class):
+        self.predicted_class = predicted_class
+        self.feature_index = 0
+        self.threshold = 0
+        self.left = None
+        self.right = None
+
+
+class DecisionTreeClassifier(object):
+    def __init__(self, max_depth=None):
+        self.max_depth = max_depth
+
+    def best_split(self, X, y):
+        '''
+        find the best split for the node
+        :param X:
+        :param y:
+        :return:
+        '''
+        m = y.size
+        if m <= 1:
+            return None, None
+        num_parent = [np.sum(y == c) for c in range(self.n_classes)]
+        best_gini = 1.0 - sum((n / m)**2 for n in num_parent)
+        best_idx, best_thr = None, None
+        for idx in range(self.n_fetures):
+            thresholds, classes = zip(*sorted(zip(X[:, idx], y)))
+
+        num_left = [0] * self.n_classes
+        num_right = num_parent.copy()
+        for i in range(1, m):
+            c = classes[i - 1]
+            num_left[c] += 1
+            num_right[c] -= 1
+            gini_left = 1.0 - \
+                sum((num_left[x] / i)**2 for x in range(self.n_classes))
+            gini_right = 1.0 - \
+                sum((num_right[x] / (m - i)) ** 2 for x in range(self.n_classes))
+            gini = (i * gini_left + (m - i) * gini_right) / m
+            if thresholds[i] == thresholds[i - 1]:
+                continue
+            if gini < best_gini:
+                best_gini = gini
+                best_idx = idx
+                best_thr = (thresholds[i] + thresholds[i - 1]) / 2
+        return best_idx, best_thr
+
+    def fit(self,X,y):
+        self.n_classes=len(set(y))
+        self.n_fetures=X.shape[1]
+        self.tree=self.grow_tree(X,y)
+
+    def grow_tree(self,X,y,depth=0):
+        #统计每个节点最终的个数
+        num_samples_per_class=[np.sum(y==i) for i in range(self.n_classes)]
+        predicted_class=np.argmax(num_samples_per_class)
+        node=Node(predicted_class=predicted_class)
+        if depth<self.max_depth:
+            idx,thr=self.best_split(X,y)
+            if idx is not None:
+                indices_left=X[:,idx]<thr
+                x_left,y_left=X[indices_left],y[indices_left]
+                x_right,y_right=X[~indices_left],y[~indices_left]
+                node.feature_index=idx
+                node.threshold=thr
+                node.left=self.grow_tree(x_left,y_left,depth+1)
+                node.right=self.grow_tree(x_right,y_right,depth+1)
+        return node
+
+    def predict(self,inputs):
+        node=self.tree
+        while node.left:
+            if inputs[node.feature_index]<node.threshold:
+                node=node.left
+            else:
+                node=node.right
+        return node.predicted_class
+
+if __name__=='__main__':
+    from sklearn.datasets import load_iris
+    datasets=load_iris()
+    X,y=datasets.data,datasets.target
+    clf=DecisionTreeClassifier(max_depth=2)
+    clf.fit(X,y)
+    print(clf.predict([[0,0,5,1.5]]))
--- GCN/intro_GCN.py
+++ GCN/intro_GCN.py
@@ -1,15 +1,44 @@
 import numpy as np
-A = np.matrix([[0, 1, 0, 0],
-               [0, 0, 1, 1],
-               [0, 1, 0, 0],
-               [1, 0, 1, 0]], dtype=float)
+# A = np.matrix([[0, 1, 0, 0],
+#                [0, 0, 1, 1],
+#                [0, 1, 0, 0],
+#                [1, 0, 1, 0]], dtype=float)
+#
+# X = np.matrix([[i, -i] for i in range(A.shape[0])], dtype=float)
+# print(X)
+# print(A*X)
+# I=np.matrix(np.eye(A.shape[0]))
+# A_HAT=A+I
+# print(A_HAT*X)
+# #0是列，1是行
+# D=np.array(np.sum(A,axis=0))[0]
+# D=np.matrix(np.diag(D))
 
-X = np.matrix([[i, -i] for i in range(A.shape[0])], dtype=float)
-print(X)
-print(A*X)
-I=np.matrix(np.eye(A.shape[0]))
-A_HAT=A+I
-print(A_HAT*X)
-D=np.array(np.sum(A,axis=0))[0]
-D=np.matrix(np.diag(D))
+# print(D_HAT)
+# print(D**-1*A*X)
+#
+# W=np.matrix([[1,-1],[-1,1]])
+# relu函数作用
+# print(np.maximum(D_HAT**-1*A_HAT*X*W,0))
+from networkx import karate_club_graph, to_numpy_matrix
+zkc = karate_club_graph()
+order = sorted(list(zkc.nodes()))
+A = to_numpy_matrix(zkc, nodelist=order)
+I = np.eye(zkc.number_of_nodes())
+A_HAT = A + I
+D_HAT = np.array(np.sum(A_HAT, axis=0))[0]
+D_HAT = np.matrix(np.diag(D_HAT))
+w_1 = np.random.normal(loc=0, scale=1, size=(zkc.number_of_nodes(), 4))
+w_2 = np.random.normal(loc=0, size=(w_1.shape[1], 2))
 
+
+def gcn_layer(A_HAT, D_HAT, X, W):
+    return np.maximum(D_HAT**-1 * A_HAT * X * W, 0)
+
+
+H_1 = gcn_layer(A_HAT, D_HAT, I, w_1)
+H_2 = gcn_layer(A_HAT, D_HAT, H_1, w_2)
+output = H_2
+feature_representations = {node: np.array(
+    output)[node] for node in zkc.nodes()}
+print(feature_representations)
